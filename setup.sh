537984ad6df69f41facc361dc853d95c5c1f9b87
ROBO358 <18186312+ROBO358@users.noreply.github.com>
2022-05-11 14:14:12 +0900

    Fix:ダウンロードが行われないバグの修正
    ダウンロード元のpath及び、権限の整理

--- setup.sh
+++ setup.sh
@@ -7,16 +7,16 @@ sudo apt update
 sudo apt -y upgrade
 
 sudo apt -y install neovim curl
-curl -sL install-node.vercel.app/lts | bash
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sudo sh -c "curl -sL install-node.vercel.app/lts | bash"
+sudo sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 sudo apt -y autoremove
 sudo apt -y autoclean
 
-cp -frb ./bin/.config $HOME/.config
+cp -frb ./bin/.config $HOME
 
 mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerdfonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 echo "Done"
 
 echo "======"
