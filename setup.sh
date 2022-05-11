a4c54cde3f3094d5043da8405ad833685e420071
ROBO358 <18186312+ROBO358@users.noreply.github.com>
2022-05-11 23:32:22 +0900

    Fix:'vim-plug'のダウンロード出来てなかった
    sudo変にかかって実行が上手く行ってなかった

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo apt update
 
 sudo apt -y install neovim curl
 sudo sh -c "curl -sL install-node.vercel.app/lts | bash"
-sudo sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 sudo apt -y autoremove
 sudo apt -y autoclean
